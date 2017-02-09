0058fabe5c1fdd082af42540708983fc855693dd
XtheWiz <xthewiz.dev@gmail.com>
2017-02-09 20:52:57 +0700

    Modified addReminder()

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -5,20 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 //Query Builder
-use DB;
+// use DB;
+
+//Eloquent
+use App\Reminder;
 
 class ReminderController extends Controller
 {
     public function home() {
-      $reminders = DB::table('reminder')->orderBy('id', 'dsc')->get();
+      // $reminders = DB::table('reminder')->orderBy('id', 'dsc')->get();
+      $reminders = Reminder::orderBy('id', 'desc')->get();
 
       return view('home', ['reminders' => $reminders]);
     }
 
     public function addReminder(Request $request) {
-      $body = $request->reminder;
+      // $body = $request->reminder;
+      $reminder = new Reminder();
 
-      DB::table('Reminder')->insert(['body'=>$body, 'isFinished'=>false, 'createdUserID'=>2]);
+      $reminder->body = $request->reminder;
+      $reminder->isFinished = false;
+      $reminder->createdUserID = 1;
+
+      $reminder->save();
+      // DB::table('Reminder')->insert(['body'=>$body, 'isFinished'=>false, 'createdUserID'=>2]);
 
       return back();
     }
