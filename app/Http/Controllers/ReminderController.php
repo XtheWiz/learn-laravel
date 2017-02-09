09384e3cb2f3b32811a48f56467d100641e8b104
XtheWiz <xthewiz.dev@gmail.com>
2017-02-09 20:49:37 +0700

    First step of Eloquent, add Reminder model

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+//Query Builder
 use DB;
 
 class ReminderController extends Controller
---
+++ app/Reminder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reminder extends Model
+{
+    protected $table = "Reminder";
+    public $primaryKey = "id";
+    public $timestamp = true;
+}
