132ac98cd949e09821792bcd46344ba742537a0c
XtheWiz <xthewiz.dev@gmail.com>
2017-02-17 14:53:31 +0700

    Finish Day6, add validation to NewReminderType

--- app/Http/Controllers/ReminderTypeController.php
+++ app/Http/Controllers/ReminderTypeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
 
 use App\ReminderType;
 
 class ReminderTypeController extends Controller
 {
     public function addReminderType(Request $request) {
+
+      $this->validate($request, [
+        'name' => 'required|unique:ReminderType,typename'
+      ]);
+
       $typeName = $request->name;
 
       $reminderType = new ReminderType();
